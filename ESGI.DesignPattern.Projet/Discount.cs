1936e935a045121974cfe94217c2ac4ea692e0c9
N-injin <jimbienvenu@gmail.com>
2020-07-01 15:20:51 +0200

    Refactor Discount

--- ESGI.DesignPattern.Projet/Discount.cs
+++ ESGI.DesignPattern.Projet/Discount.cs
@@ -4,22 +4,45 @@ using System.Runtime.Serialization;
 
 namespace ESGI.DesignPattern.Projet
 {
+    public class DiscountCommand
+    {
+        public IMoney ApplyDiscount(IMarketingCampaign campaign, IMoney money)
+        {
+            IDiscount discount = null;
+
+            if (campaign.IsCrazySalesDay())
+            {
+                discount = new DiscountCrazySalesDay();
+            } 
+            else if (money.IsMoreThanOneThousand())
+            {
+                discount = new DiscountOneThousand();
+            }
+            else if (money.IsMoreThanOneHundred() && campaign.IsActive())
+            {
+                discount = new DiscountOneHundred();
+            }
+
+            return discount != null ? discount.DiscountFor(money) : money;
+        }
+    }
+
     public interface IDiscount
     {
-        Money DiscountFor(Money netPrice);
+        IMoney DiscountFor(IMoney netPrice);
     }
 
     public class DiscountOneThousand : IDiscount
     {
-        public Money DiscountFor(Money netPrice)
+        public IMoney DiscountFor(IMoney netPrice)
         {
             return netPrice.ReduceBy(10);
         }
     }
 
-    public class DiscountBlackFriday : IDiscount
+    public class DiscountCrazySalesDay : IDiscount
     {
-        public Money DiscountFor(Money netPrice)
+        public IMoney DiscountFor(IMoney netPrice)
         {
             return netPrice.ReduceBy(15);
         }
@@ -27,37 +50,9 @@ namespace ESGI.DesignPattern.Projet
 
     public class DiscountOneHundred : IDiscount
     {
-        public Money DiscountFor(Money netPrice)
+        public IMoney DiscountFor(IMoney netPrice)
         {
             return netPrice.ReduceBy(5);
-        }
-    }
-
-    // Old code
-    public class Discount
-    {
-        private readonly MarketingCampaign marketingCampaign;
-
-        public Discount(MarketingCampaign marketingCampaign) // pass IMarketingCampaign
-        {
-            this.marketingCampaign = marketingCampaign;
-        }
-
-        public Money DiscountFor(Money netPrice) // pass IMoney
-        {
-            if (marketingCampaign.IsCrazySalesDay())
-            {
-                return netPrice.ReduceBy(15);
-            }
-            if (netPrice.MoreThan(Money.OneThousand))
-            {
-                return netPrice.ReduceBy(10);
-            }
-            if (netPrice.MoreThan(Money.OneHundred) && marketingCampaign.IsActive())
-            {
-                return netPrice.ReduceBy(5);
-            }
-            return netPrice;
         }
     }
 }
